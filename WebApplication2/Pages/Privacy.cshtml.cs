8e7054613d6e8190f81f08fd6791b63ffa0b246b
Achilles911 <lolololo.lolo12345@mail.ru>
2024-03-20 02:54:55 +0300

    Ничего интеренсого

--- WebApplication2/Pages/Privacy.cshtml.cs
+++ WebApplication2/Pages/Privacy.cshtml.cs
@@ -1,8 +1,10 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -22,3 +24,27 @@ namespace WebApplication2.Pages
         }
     }
 }
+CREATE TABLE[dbo].[Assets] (
+    [Id]                  INT NOT NULL,
+    [object_name]         TEXT         NULL,
+    [inventory_number]    INT          NULL,
+    [quantity]            INT          NULL,
+    [year_introduction]   INT          NULL,
+    [initial_cost]        DECIMAL (18) NULL,
+    [residual_value]      DECIMAL (18) NULL,
+    [useful_life]         INT          NULL,
+    [technical_condition] TEXT         NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
+CREATE TABLE[dbo].[Users] (
+    [Id]          INT NOT NULL,
+    [fullname]    NVARCHAR (MAX) NULL,
+    [email]       NVARCHAR (MAX) NULL,
+    [password]    NVARCHAR (MAX) NULL,
+    [phonenumber] DECIMAL (18)   NULL,
+    [role]        NVARCHAR (MAX) NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
+
