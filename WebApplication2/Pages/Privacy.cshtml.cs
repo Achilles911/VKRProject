eed760d2895cebfdedfb14800400a0c3af732bc2
Илья Шиманов <ilya.shimanov@internet.ru>
2024-03-20 03:05:56 +0300

    Фигня случается

--- WebApplication2/Pages/Privacy.cshtml.cs
+++ WebApplication2/Pages/Privacy.cshtml.cs
@@ -24,27 +24,8 @@ namespace WebApplication2.Pages
         }
     }
 }
-CREATE TABLE[dbo].[Assets] (
-    [Id]                  INT NOT NULL,
-    [object_name]         TEXT         NULL,
-    [inventory_number]    INT          NULL,
-    [quantity]            INT          NULL,
-    [year_introduction]   INT          NULL,
-    [initial_cost]        DECIMAL (18) NULL,
-    [residual_value]      DECIMAL (18) NULL,
-    [useful_life]         INT          NULL,
-    [technical_condition] TEXT         NULL,
-    PRIMARY KEY CLUSTERED ([Id] ASC)
-);
 
-CREATE TABLE[dbo].[Users] (
-    [Id]          INT NOT NULL,
-    [fullname]    NVARCHAR (MAX) NULL,
-    [email]       NVARCHAR (MAX) NULL,
-    [password]    NVARCHAR (MAX) NULL,
-    [phonenumber] DECIMAL (18)   NULL,
-    [role]        NVARCHAR (MAX) NULL,
-    PRIMARY KEY CLUSTERED ([Id] ASC)
-);
+
+
 
 
